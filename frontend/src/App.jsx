00d48c504904e144670db07719ff46c0516e51a7
Maulik80 <maulikgandhi80@gmail.com>
2025-12-28 12:01:27 +0530

    Add meta img

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,17 @@ import { NotFound } from "./pages/NotFound";
 import { Toaster } from "./components/toaster"; 
 
 function App() {
-  return (
-    <>
-      <Toaster />
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+ return (
+  <>
+ <Toaster />
+ <BrowserRouter>
+ <Routes>
+ <Route index element={<Home />} />
+ <Route path="*" element={<NotFound />} />
+ </Routes>
+ </BrowserRouter>
+ </>
+ );
 }
 
 export default App;
